9a251e92658703857d2dfae6045543d40cdb8465
altafpathan65012 <137278745+altafpathan65012@users.noreply.github.com>
2024-09-02 16:07:17 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,10 +2,10 @@ import os
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", 25218674))
-API_HASH = getenv("API_HASH", "87c231f0e8704795b8239d06965b4351")
-BOT_TOKEN = getenv("BOT_TOKEN", "7198600769:AAGW4WAlCWppTjwTceBxWqDwpWP7UCEWuzI")
-OWNER_ID = int(getenv("OWNER_ID", "6805344720"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "999741495  20000898623").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002038285084"))
+API_ID = int(getenv("API_ID", 23031620))
+API_HASH = getenv("API_HASH", "31cb00c1cbe580394778b43105864bca")
+BOT_TOKEN = getenv("BOT_TOKEN", "6865731231:AAEDdXn48VOzOf0jRo0CvxkU0VxIFsikTvg")
+OWNER_ID = int(getenv("OWNER_ID", "502980590"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "502980590  20000898623").split()))
+MONGO_URL = getenv("MONGO_DB", "mongodb+srv://altafpathan65012:<password>@cluster0.aztmome.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002155787742"))
